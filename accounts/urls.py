13fffc600dd69228e7c6cc6c9f1b9a8c56d825b7
lnascimento <almeida.luan099@gmail.com>
2018-11-27 09:59:18 -0200

    Criação das rotas de accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.dashboard,name='dashboard'),
+    
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('register',views.register,name='register')
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
+
+def register(request):
+    return HttpResponse("Register")
+
+def login(request):
+    return HttpResponse("Login")
+
+def logout(request):
+    return HttpResponse("Logout")
+
