18a528ae500104836d38b9777b7942e2b042a179
luan <almeida.luan099@gmail.com>
2018-11-28 18:00:08 -0200

    Alert para erros no formulario de registro

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
 def register(request):
     if request.method == 'POST':
-        print('First Name : {} '.format(request.POST['first_name']))
+        password1 = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password1 == password2: 
+           pass
+        else:
+            messages.error(request,'Senhas nao sao iguais')
         return redirect('register')
     else:
         return render(request,'accounts/register.html')
