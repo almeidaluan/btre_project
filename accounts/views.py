7e7897a359940a2fbe28fb403d7d390777a2d9df
luan <almeida.luan099@gmail.com>
2018-11-28 19:55:58 -0200

    Validacao do formulario com redirect login

--- accounts/views.py
+++ accounts/views.py
@@ -7,20 +7,49 @@ def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
 def register(request):
-    if request.method == 'POST':
-        password1 = request.POST.get('password')
-        password2 = request.POST.get('password2')
-        if password1 == password2: 
-           pass
-        else:
-            messages.error(request,'Senhas nao sao iguais')
+  if request.method == 'POST':
+    # Get form values
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    # Check if passwords match
+    if password == password2:
+      # Check username
+      if User.objects.filter(username=username).exists():
+        messages.error(request, 'That username is taken')
         return redirect('register')
+      else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request, 'That email is being used')
+          return redirect('register')
+        else:
+          # Looks good
+          user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
+          # Login after register
+          # auth.login(request, user)
+          # messages.success(request, 'You are now logged in')
+          # return redirect('index')
+          user.save()
+          messages.success(request, 'You are now registered and can log in')
+          return redirect('login')
     else:
-        return render(request,'accounts/register.html')
+      messages.error(request, 'Passwords do not match')
+      return redirect('register')
+  else:
+    return render(request, 'accounts/register.html')
 
 def login(request):
     return render(request,'accounts/login.html')
 
 def logout(request):
     return HttpResponse("Logout")
+
+
+
+
+    
 
